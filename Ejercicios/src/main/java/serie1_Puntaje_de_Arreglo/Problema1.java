9def29b582c9e0029f93ff8c878899cb1a030fdf
Rafael <jpereza62@miumg.edu.gt>
2026-03-14 14:10:38 -0600

    Primer commit

---
+++ Ejercicios/src/main/java/serie1_Puntaje_de_Arreglo/Problema1.java
@@ -0,0 +1,14 @@
+package serie1_Puntaje_de_Arreglo;
+
+public class Problema1 {
+	public static int points(int[] numbers) {
+		int total = 0;
+		
+		for(int num: numbers) {
+			
+		}
+		
+		return total;
+	}
+	
+}
