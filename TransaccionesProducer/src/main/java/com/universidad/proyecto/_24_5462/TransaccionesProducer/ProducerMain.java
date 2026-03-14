7d596f27bd55f17ca47c3d1708a20d44569b9ace
Rafael <jpereza62@miumg.edu.gt>
2026-03-14 15:47:36 -0600

    Decimo commit

--- TransaccionesProducer/src/main/java/com/universidad/proyecto/_24_5462/TransaccionesProducer/ProducerMain.java
+++ TransaccionesProducer/src/main/java/com/universidad/proyecto/_24_5462/TransaccionesProducer/ProducerMain.java
@@ -5,8 +5,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -30,28 +33,24 @@ public class ProducerMain {
             System.out.println("Iniciando Producer...");
 
             try {
-                HttpRequest request = HttpRequest.newBuilder() 
-                        .uri(URI.create(url))
-                        .GET()
-                        .build();// Construimos la solicitud HTTP para obtener las transacciones
-
-                System.out.println("Consultando API...");
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());// Enviamos la solicitud y obtenemos la respuesta
+                HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
                 if (response.statusCode() == 200) {
                     LoteTransacciones lote = mapper.readValue(response.body(), LoteTransacciones.class);
-                    System.out.println("Lote recibido: " + lote.getLoteId() + " con " + lote.getTransacciones().size() + " transacciones.");
+                    
                     
                     channel.queueDeclare("cola_duplicados", true, false, false, null);
-                    System.out.println(" [x] Cola 'cola_duplicados' declarada y lista.");
+                    channel.queueDeclare("cola_errores", true, false, false, null); 
+
                     
+                    Map<String, Object> queueArgs = new HashMap<>();
+                    queueArgs.put("x-max-priority", 10); 
+
                     int transaccionesEnviadas = 0;
 
                     for (Transaccion tx : lote.getTransacciones()) { // Iteramos sobre cada transacción del lote y se asignan los datos
-                        if (transaccionesEnviadas >= 100) {// si son más de 100 transacciones, se detiene el proceso
-                            break;
-                        }
-
+                        if (transaccionesEnviadas >= 100) break; // si son más de 100 transacciones, se detiene el proceso
                      
                         tx.setNombreEstudiante("Josué Rafael Pérez Aguirre"); 
                         tx.setCarnetEstudiante("0905-24-5462");  
@@ -59,24 +58,32 @@ public class ProducerMain {
                         
                         String nombreCola = tx.getBancoDestino();
                         
-                        channel.queueDeclare(nombreCola, true, false, false, null); // Declaramos la cola con el nombre del banco destino, si no existe se crea, si ya existe se usa
+                        
+                        channel.queueDeclare(nombreCola, true, false, false, queueArgs); 
+
+                       
+                        int prioridad = (tx.getMonto() > 5000) ? 10 : 1;
+
+                        AMQP.BasicProperties props = new AMQP.BasicProperties.Builder()
+                                .priority(prioridad)
+                                .build();
 
                         String jsonMensaje = mapper.writeValueAsString(tx);
                         
-                        channel.basicPublish("", nombreCola, null, jsonMensaje.getBytes(StandardCharsets.UTF_8));// Publicamos el mensaje en la cola correspondiente al banco destino
+                        
+                        channel.basicPublish("", nombreCola, props, jsonMensaje.getBytes(StandardCharsets.UTF_8));
+                        
+                        
                         System.out.println(" [v] Transacción " + tx.getIdTransaccion() + " enviada a cola: " + nombreCola);
                         
                         transaccionesEnviadas++;
                     }
                     System.out.println("\nProceso finalizado. Se enviaron exitosamente " + transaccionesEnviadas + " transacciones.");
-                } else {
-                    System.err.println("Error en API: Status " + response.statusCode());
                 }
 
             } catch (Exception e) {
                 System.err.println("Error durante el procesamiento: " + e.getMessage());
             }
-            
         } catch (Exception e) {
             System.err.println("Error crítico de conexión: " + e.getMessage());
         }
