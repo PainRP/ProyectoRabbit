edee57f02184ae08d658bc0360bc0d493cf7f6a3
Rafael <jpereza62@miumg.edu.gt>
2026-03-14 14:20:12 -0600

    Segundo commit

--- Ejercicios/src/main/java/serie1_Puntaje_de_Arreglo/Problema1.java
+++ Ejercicios/src/main/java/serie1_Puntaje_de_Arreglo/Problema1.java
@@ -5,10 +5,18 @@ public class Problema1 {
 		int total = 0;
 		
 		for(int num: numbers) {
-			
+			if(num%2==0 || num == 0) {
+				total = total+1;
+			}else if(num == 5) {
+				total = total +5;
+			} else {
+				total = total+3;
+			}
 		}
 		
 		return total;
 	}
 	
+	//La complejidad temporal sería de O(n), debido a que es un bucle for-each
+	//La complejidad espacial sería de O(1), debido a que no se crea nada más, solo una variabble
 }
---
+++ Ejercicios/src/main/java/serie1_Puntaje_de_Arreglo/Problema2.java
@@ -0,0 +1,14 @@
+package serie1_Puntaje_de_Arreglo;
+
+public class Problema2 {
+	public static int[] secondMinMax(int[] numbers) {
+		int[] secondMINMAX = new int[2];
+		
+		for(int i: numbers) {
+			
+		}
+		
+		
+		return secondMINMAX;
+	}
+}
